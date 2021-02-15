6e8b6547471a0c4ecbdb3068a8dc1815e2453ae1
Aamir <aamirp1@outlook.com>
2021-02-15 22:17:50 +0000

    implemented REST API in node using Express

---
+++ part3/phonebook/index.js
@@ -0,0 +1,96 @@
+const e = require('express');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const morgan = require('morgan');
+morgan('tiny');
+
+morgan.token('body', (request) => {
+  if (request.method == 'POST') return ' ' + JSON.stringify(request.body);
+  else return ' ';
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+let persons = [
+  {
+    id: 1,
+    name: 'Arto',
+    number: '1231',
+  },
+  {
+    id: 2,
+    name: 'Dan',
+    number: '0982',
+  },
+  {
+    id: 3,
+    name: 'Chris Hem',
+    number: '7221',
+  },
+];
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+});
+
+app.get('/info', (req, res) => {
+  const numberOfPersons = persons.length;
+  const date = new Date();
+  res.send(`<p>Phonebook has info for ${numberOfPersons} people.</p> 
+  <p>${date}</p>`);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  res.status(204).end();
+});
+
+const generateId = () => {
+  return Math.floor(Math.random() * 10000000000000);
+};
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+  const name = persons.find((person) => person.name === body.name);
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'name or number is missing',
+    });
+  } else if (name) {
+    return res.status(409).json({
+      error: 'name must be unique',
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  res.json(person);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
