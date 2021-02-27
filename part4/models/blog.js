5d5d4f4361095dfc2b7caf9bac78b6a3af776b2e
Aamir <aamirp1@outlook.com>
2021-02-27 16:00:24 +0000

    4.15

--- part4/models/blog.js
+++ part4/models/blog.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose')
-mongoose.set('useFindAndModify', false)
 
-const blogSchema = mongoose.Schema({
-  title: { type: String, required: true, minlenght: 5 },
-  author: { type: String, required: true, minlenght: 5 },
-  url: String,
-  likes: Number,
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: String,
+  url: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
 })
 
 blogSchema.set('toJSON', {
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,75 +1,132 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
 
 const api = supertest(app)
-
-const Blog = require('../models/Blog')
-
-const initialBlogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0,
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0,
-  },
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0,
-  },
-  {
-    _id: '5a422b891b54a676234d17fa',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-    __v: 0,
-  },
-  {
-    _id: '5a422ba71b54a676234d17fb',
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-    __v: 0,
-  },
-  {
-    _id: '5a422bc61b54a676234d17fc',
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-    __v: 0,
-  },
-]
 
 beforeEach(async () => {
   await Blog.deleteMany({})
 
-  for (let blog of initialBlogs) {
-    let obj = new Blog(blog)
-    await obj.save()
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
   }
 })
+describe('get requests tests', () => {
+  test('blogs are returned as JSON', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('all blogs returned as json', async () => {
-  const response = await api.get('/api/blogs').expect('Content-Type', /application\/json/)
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
 
-  expect(response.body).toHaveLength(6)
+  test('a specific blog is within returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map((blog) => blog.title)
+
+    expect(titles).toContain('React is awesome')
+  })
+
+  test('is property equal to id, instead of _id', async () => {
+    const response = await api.get('/api/blogs')
+    const ids = response.body.map((blog) => blog.id)
+    expect(ids).toBeDefined()
+  })
+})
+
+describe('post requests tests', () => {
+  test('a valid blog can be added to database', async () => {
+    const newBlog = {
+      title: 'FulstackOpen is the best way to practice all aspects of programming',
+      author: 'Mariusz Pudzianowski',
+      url: 'www.pudzian.com',
+      likes: 44,
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    const blogTitles = response.body.map((blog) => blog.title)
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+    expect(blogTitles).toContain('FulstackOpen is the best way to practice all aspects of programming')
+  })
+
+  test('has the request likes property', async () => {
+    const newBlog = {
+      title: 'This blog will not have likes',
+      author: 'Lacking Like',
+      url: 'www.nolikes.com',
+    }
+
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    expect(response.body.likes).toBeFalsy()
+  })
+
+  test('blog has title and url missing', async () => {
+    const newBlog = {
+      author: 'Missing TitleAndUrl',
+      likes: 30,
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+  })
+})
+
+describe('delete requests tests', () => {
+  test('deleting single blog', async () => {
+    const blogsAtStart = await helper.blogsInDatabase()
+    const blogToDelete = blogsAtStart[0]
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+    const blogsAtEnd = await helper.blogsInDatabase()
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
+
+    const titles = blogsAtEnd.map((blog) => blog.title)
+    expect(titles).not.toContain(blogToDelete.title)
+  })
+})
+
+describe('put requests tests', () => {
+  test('updating single blog', async () => {
+    const blogsAtStart = await helper.blogsInDatabase()
+    const blogToUpdate = blogsAtStart[0]
+
+    const newBlog = {
+      title: 'New updated blog title',
+      author: 'Put Tester',
+      url: 'www.putIsWorking.com',
+      likes: 999,
+    }
+
+    await api.put(`/api/blogs/${blogToUpdate.id}`).send(newBlog)
+
+    const blogsAtEnd = await helper.blogsInDatabase()
+    // console.log(blogsAtEnd)
+    const titles = blogsAtEnd.map((blog) => blog.title)
+    expect(titles).toContain('New updated blog title')
+  })
 })
 
 afterAll(() => {
