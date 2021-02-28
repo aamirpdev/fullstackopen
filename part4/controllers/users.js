7b289d99e876328cd5e011dd60a240821f4d2682
Aamir <aamirp1@outlook.com>
2021-02-28 02:46:39 +0000

    4.16

---
+++ part4/controllers/users.js
@@ -0,0 +1,31 @@
+const bcrypt = require('bcrypt')
+const userRouter = require('express').Router()
+const User = require('../models/user')
+
+userRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  if (body.password.length < 3 || body.password === undefined) {
+    return response.status(400).json({ error: 'password must be longer than 3 characters' })
+  }
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+  const user = new User({
+    username: body.username,
+    name: body.name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  response.json(savedUser)
+})
+
+userRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
+})
+
+module.exports = userRouter
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const helper = require('./test_helper')
+const bcrypt = require('bcrypt')
 
 const api = supertest(app)
 
@@ -126,6 +128,60 @@ describe('put requests tests', () => {
     // console.log(blogsAtEnd)
     const titles = blogsAtEnd.map((blog) => blog.title)
     expect(titles).toContain('New updated blog title')
+  })
+})
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map((u) => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+
+  test('creation fails with proper statuscode and error message if password is invalid', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai1',
+      name: 'Matti Luukkainen1',
+      password: 'sa',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('password must be longer than 3 characters')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
   })
 })
 
