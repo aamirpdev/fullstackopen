def2ccd14891f5bd4b38191ec2eef42db5012454
Aamir <aamirp1@outlook.com>
2021-03-02 14:47:24 +0000

    4.19

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -3,16 +3,8 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
   response.json(blogs.map((blog) => blog.toJSON()))
 })
 
@@ -24,6 +16,14 @@ blogRouter.get('/:id', async (request, response) => {
     response.status(404).end()
   }
 })
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogRouter.post('/', async (request, response) => {
   const body = request.body
